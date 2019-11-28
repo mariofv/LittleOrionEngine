70a5e4407247ad002cc367716c154d1bbe657f3f
Mario Fernández Villalba <mario_bcn2@hotmail.es>
2019-11-28 12:54:19 +0100

    Added build creator

---
+++ BuildCreator.ps1
@@ -0,0 +1,8 @@
+Remove-Item -LiteralPath "Build" -Force -Recurse
+Remove-Item -LiteralPath "Build.zip" -Force -Recurse
+
+
+Copy-Item -Path Game -Destination Build -Recurse -Force -Container
+Copy-Item Release\OrionEngine.exe -Destination Build
+
+Compress-Archive -Path Build -DestinationPath Build.zip
