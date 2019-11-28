3a55f0dea447aaa19d80348287a1ac183b25083f
Mario Fernández Villalba <mariobcn3@gmail.com>
2019-11-28 12:55:07 +0100

    Update BuildCreator.ps1

--- BuildCreator.ps1
+++ BuildCreator.ps1
@@ -1,7 +1,6 @@
 Remove-Item -LiteralPath "Build" -Force -Recurse
 Remove-Item -LiteralPath "Build.zip" -Force -Recurse
 
-
 Copy-Item -Path Game -Destination Build -Recurse -Force -Container
 Copy-Item Release\OrionEngine.exe -Destination Build
 
