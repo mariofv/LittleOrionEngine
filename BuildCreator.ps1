d9409510268cdea773c7dfa073bfe093527206d2
mariofv <mariobcn3@gmail.com>
2019-12-03 18:39:53 +0100

    Changed BuildCreator with new project name

--- BuildCreator.ps1
+++ BuildCreator.ps1
@@ -2,6 +2,6 @@ Remove-Item -LiteralPath "Build" -Force -Recurse
 Remove-Item -LiteralPath "Build.zip" -Force -Recurse
 
 Copy-Item -Path Game -Destination Build -Recurse -Force -Container
-Copy-Item Release\OrionEngine.exe -Destination Build
+Copy-Item Release\LittleOrionEngine.exe -Destination Build
 
 Compress-Archive -Path Build -DestinationPath Build.zip
