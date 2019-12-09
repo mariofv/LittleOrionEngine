5f346d049b8afa9ab5388cf406025623e8c56110
Anabel <48101863+yalania@users.noreply.github.com>
2019-12-09 18:02:35 +0100

    add incons to folders

--- Engine/UI/FileExplorerUI.cpp
+++ Engine/UI/FileExplorerUI.cpp
@@ -16,18 +16,19 @@ void FileExplorerUI::ShowAssetsFolders() {
 
 void FileExplorerUI::WindowShowFilesInFolder(const char * path) {
 
-	WIN32_FIND_DATA FindFileData;
-	HANDLE hFind = FindFirstFile(path, &FindFileData);
-	if (hFind == INVALID_HANDLE_VALUE) {;
+	WIN32_FIND_DATA find_file_data;
+	HANDLE handle_find = FindFirstFile(path, &find_file_data);
+	if (handle_find == INVALID_HANDLE_VALUE) {;
 		return;
 	}
 	do {
-		if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY && std::strcmp(FindFileData.cFileName,".")  && std::strcmp(FindFileData.cFileName, ".."))
+		if (find_file_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY && std::strcmp(find_file_data.cFileName,".")  && std::strcmp(find_file_data.cFileName, ".."))
 		{
 			ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_SpanFullWidth | ImGuiTreeNodeFlags_DefaultOpen;
 			size_t last_slash_position = std::string(path).find_last_of("//");
-			std::string new_path = std::string(path).substr(0, last_slash_position) +"/"+std::string(FindFileData.cFileName)+ "//*";
+			std::string new_path = std::string(path).substr(0, last_slash_position) +"/"+std::string(find_file_data.cFileName)+ "//*";
 			int subfolders = GetWindowsSubFolders(new_path.c_str());
+			std::string filename = ICON_FA_FOLDER " "+std::string(find_file_data.cFileName);
 			if (subfolders == 0)
 			{
 				flags |= ImGuiTreeNodeFlags_Leaf;
@@ -35,34 +36,35 @@ void FileExplorerUI::WindowShowFilesInFolder(const char * path) {
 			if (selected_folder == new_path)
 			{
 				flags |= ImGuiTreeNodeFlags_Selected;
+				filename = ICON_FA_FOLDER_OPEN " " + std::string(find_file_data.cFileName);
 			}
-			if (ImGui::TreeNodeEx(FindFileData.cFileName,flags)) 
+			if (ImGui::TreeNodeEx(filename.c_str(),flags))
 			{
 				ProcessMouseInput(new_path);
 				WindowShowFilesInFolder(new_path.c_str());
 				ImGui::TreePop();
 			}
 		}
-	} while (FindNextFile(hFind, &FindFileData) != 0);
+	} while (FindNextFile(handle_find, &find_file_data) != 0);
 
-	FindClose(hFind);
+	FindClose(handle_find);
 
 }
 
 int FileExplorerUI::GetWindowsSubFolders(const char * path) {
-	WIN32_FIND_DATA FindFileData;
-	HANDLE hFind = FindFirstFile(path, &FindFileData);
+	WIN32_FIND_DATA find_file_data;
+	HANDLE handle_find = FindFirstFile(path, &find_file_data);
 
-	if (hFind == INVALID_HANDLE_VALUE) {
+	if (handle_find == INVALID_HANDLE_VALUE) {
 		return 0;
 	}
 	int subfolders = 0;
 	do {
-		if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY && std::strcmp(FindFileData.cFileName, ".") && std::strcmp(FindFileData.cFileName, ".."))
+		if (find_file_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY && std::strcmp(find_file_data.cFileName, ".") && std::strcmp(find_file_data.cFileName, ".."))
 		{
 			subfolders++;
 		}
-	} while (FindNextFile(hFind, &FindFileData) != 0);
+	} while (FindNextFile(handle_find, &find_file_data) != 0);
 
 	return subfolders;
 }
