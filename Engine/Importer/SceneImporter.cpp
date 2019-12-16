c32073e2e95d4da7a4ae61563192686f9fc711c4
Anabel <48101863+yalania@users.noreply.github.com>
2019-12-16 21:10:14 +0100

    load json

--- Engine/Importer/SceneImporter.cpp
+++ Engine/Importer/SceneImporter.cpp
@@ -1,18 +1,35 @@
 #include "SceneImporter.h"
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
 
 void SceneImporter::LoadImportedFilesRegister() const
 {
 	char* buffer;
 	App->filesystem->Load(REGISTER_PATH, &buffer);
-	std::string full_register = std::string(buffer);
+	std::string full_register_json = std::string(buffer);
+
+	rapidjson::Document json_document;
+	json_document.Parse(full_register_json.c_str());
+
+	rapidjson::Value& results = json_document[0];
+	assert(results.IsArray());
+	for (rapidjson::SizeType i = 0; i < results.Size(); i++) {
+		// Store the value of the element in a vector.
+		std::string assets_object = std::string(results[i][0].GetString());
+		std::string library_object = std::string(results[i][1].GetString());
+		//imported_file_register.emplace(assets_object, library_object);
+	}
+
 }
 
 void SceneImporter::SaveImportedFilesRegister() const
 {
-	std::string full_register;
+	std::string full_register_json;
 	for (auto & file_register : imported_file_register)
 	{
-		full_register += file_register.first + "," + file_register.second + "\n";
+		full_register_json += "\""+ file_register.first+ "\"" + ":" + "\"" +file_register.second + "\"" + ",";
 	}
-	App->filesystem->Save(REGISTER_PATH, full_register.c_str(), sizeof(full_register));
+
+	App->filesystem->Save(REGISTER_PATH, full_register_json.c_str(), sizeof(full_register_json));
 }
