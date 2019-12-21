04d283f432c32d33f6bc7cbef4458ab31b9f5b80
yalania <anaisabel14hb@gmail.com>
2019-12-21 23:09:56 +0100

    avoid leaking memory

--- Engine/UI/FileExplorerUI.cpp
+++ Engine/UI/FileExplorerUI.cpp
@@ -135,8 +135,9 @@ void FileExplorerUI::ShowFileSystemActionsMenu(const ModuleFileSystem::File & fi
 				std::string new_empty_file = file.file_path + "//example.txt";
 				App->filesystem->Save(new_empty_file.c_str(), test, sizeof("This is a test text"),false);
 				size_t size;
-				char * test2 = App->filesystem->Load(new_empty_file.c_str(), size);
-				APP_LOG_INFO("Read: %s",test2);
+				char * load_test = App->filesystem->Load(new_empty_file.c_str(), size);
+				APP_LOG_INFO("Read: %s", load_test);
+				free(load_test);
 			}
 			ImGui::EndMenu();
 		}
