290bd57f52181c6c29e49974c5229327eedbd5e7
yalania <anaisabel14hb@gmail.com>
2019-12-26 12:43:14 +0100

    Add moduleLight

---
+++ Engine/Module/ModuleLight.h
@@ -0,0 +1,13 @@
+#ifndef _MODULELIGHT_H_
+#define _MODULELIGHT_H_
+#include "Module.h"
+
+class ModuleLight : public Module
+{
+public:
+	ModuleLight() = default;
+	~ModuleLight() = default;
+};
+
+#endif // !_MODULELIGHT_H_
+
