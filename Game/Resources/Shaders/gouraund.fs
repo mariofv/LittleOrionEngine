1b5acc9347dc5409ab8d900c5c60b5a48befdf68
yalania <anaisabel14hb@gmail.com>
2019-12-29 00:10:10 +0100

    Add gouraund shader

--- Game/Resources/Shaders/gouraund.fs
+++ Game/Resources/Shaders/gouraund.fs
@@ -1,8 +1,8 @@
 #version 330 core
 
+in float diffuse_intensity;
+in float specular_intensity;
 in vec2 texCoord;
-in vec3 normal;
-in vec3 FragPos;
 
 out vec4 FragColor;
 
@@ -21,44 +21,24 @@ struct Material {
 };
 uniform Material material;
 
-struct Light{
-	vec3 light_color;
-	vec3 light_position;
-	float light_intensity;
-};
-uniform Light light;
-
-uniform vec3 viewPos;
-
 vec4 get_diffuse_color(const Material mat, const vec2 texCoord);
 vec3 get_occlusion_color(const Material mat, const vec2 texCoord);
 vec3 get_emissive_color(const Material mat, const vec2 texCoord);
 vec4 get_specular_color(const Material mat, const vec2 texCoord);
-float lambert(vec3 light_position,vec3 norm);
 
 void main()
-{
+{	
     vec4 diffuse_color  = get_diffuse_color(material, texCoord);    
 	vec4 specular_color  = get_specular_color(material, texCoord);    
 	vec3 occlusion_color = get_occlusion_color(material, texCoord);    
 	vec3 emissive_color  = get_emissive_color(material, texCoord);
-  	
-    // diffuse 
-	vec3 lightDir = light.light_position - FragPos;
-	float diffuse = lambert(lightDir, normal);
-     
-	//specular
-    vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, normalize(normal));  
-    float specular = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-
+	
     vec3 result = emissive_color
 	+diffuse_color.rgb*(occlusion_color*material.k_ambient)
-	+diffuse_color.rgb*diffuse*material.k_diffuse
-	+specular_color.rgb*specular*material.k_specular;
+	+diffuse_color.rgb*diffuse_intensity
+	+specular_color.rgb*specular_intensity;
     FragColor = vec4(result,1.0);
 }
-
 
 vec4 get_diffuse_color(const Material mat, const vec2 texCoord)
 {     
@@ -76,12 +56,4 @@ vec3 get_occlusion_color(const Material mat, const vec2 texCoord)
 vec3 get_emissive_color(const Material mat, const vec2 texCoord)
 {     
 	return (texture(mat.emissive_map, texCoord)*mat.emissive_color).rgb;    
-}
-
-float lambert(vec3 direcction,vec3 norm)
-{
-  vec3 normal   = normalize(norm);
-  vec3 lightDir = normalize(direcction);
-  float diffuse = max(0.0, dot(normal, lightDir));
-  return diffuse;
 }
