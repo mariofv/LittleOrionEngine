6d83b311f8b0fc14ab2e43542b6e7c92048a74c4
yalania <anaisabel14hb@gmail.com>
2020-01-10 18:38:14 +0100

    Mor brofiler macros

--- Engine/Module/ModuleRender.cpp
+++ Engine/Module/ModuleRender.cpp
@@ -145,6 +145,7 @@ void ModuleRender::Render() const
 {
 	BROFILER_CATEGORY("Global Render",Profiler::Color::Aqua);
 	App->ui->Render();
+	BROFILER_CATEGORY("Swap Window (VSYNC)", Profiler::Color::Aquamarine);
 	SDL_GL_SwapWindow(App->window->window);
 	App->time->EndFrame();
 }
