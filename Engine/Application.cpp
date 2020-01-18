c2a3550ac99baee3e97c3316623ed8c4d7e80a9c
Mario Fernández Villalba <mario_bcn2@hotmail.es>
2020-01-18 10:29:58 +0100

    Fix mouse scroling not working on editor UI

--- Engine/Application.cpp
+++ Engine/Application.cpp
@@ -26,13 +26,13 @@ Application::Application()
 	// Order matters: they will Init/start/update in this order
 	modules.emplace_back(window = new ModuleWindow());
 	modules.emplace_back(filesystem = new ModuleFileSystem());
+	modules.emplace_back(input = new ModuleInput());
 	modules.emplace_back(ui = new ModuleUI());
 	modules.emplace_back(time = new ModuleTime());
 	modules.emplace_back(texture = new ModuleTexture());
 	modules.emplace_back(renderer = new ModuleRender());
 	modules.emplace_back(editor = new ModuleEditor());
 	modules.emplace_back(scene = new ModuleScene());
-	modules.emplace_back(input = new ModuleInput());
 	modules.emplace_back(program = new ModuleProgram());
 	modules.emplace_back(cameras = new ModuleCamera());
 	modules.emplace_back(model_loader = new ModuleModelLoader());
