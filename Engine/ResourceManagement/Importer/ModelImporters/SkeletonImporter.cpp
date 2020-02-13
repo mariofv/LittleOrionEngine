83520781a1095eb99cd9d196a4f71805dcf0bc66
Anabel Hernandez Barrera <Anabel.Hernandez@king.com>
2020-02-13 14:48:17 +0100

    create skeleton resource class

--- Engine/ResourceManagement/Importer/ModelImporters/SkeletonImporter.cpp
+++ Engine/ResourceManagement/Importer/ModelImporters/SkeletonImporter.cpp
@@ -3,9 +3,13 @@
 
 bool SkeletonImporter::ImportSkeleton(const aiScene* scene, const aiMesh* mesh, const aiMatrix4x4& mesh_transformation, const std::string& output_file) const
 {
-	for (size_t i = 0; i < 5; i++)
+	for (size_t i = 0; i < mesh->mNumBones; i++)
 	{
-
+		aiString boneName = mesh->mBones[i]->mName;
+		aiNode * bone = scene->mRootNode->FindNode(boneName);
+		int n = bone->mNumChildren;
+	
+		
 	}
 	return true;
 }
---
+++ Engine/ResourceManagement/Resources/Skeleton.h
@@ -0,0 +1,11 @@
+#ifndef _SKELETON_H_
+#define _SKELETON_H_
+#include "Resource.h"
+class Skeleton : public Resource
+{
+public:
+	Skeleton() = default;
+	~Skeleton() = default;
+};
+#endif //_SKELETON_H_
+
