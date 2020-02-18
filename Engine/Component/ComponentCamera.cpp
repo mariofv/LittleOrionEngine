cb74ab4526e5b15a3a66703bdc6b15780eb7a713
JohnnyGinard <jgm7.hd@gmail.com>
2020-02-18 19:43:04 +0100

    Improved logic

--- Engine/Component/ComponentCamera.cpp
+++ Engine/Component/ComponentCamera.cpp
@@ -28,6 +28,7 @@ ComponentCamera::~ComponentCamera()
 {
 	glDeleteTextures(1, &last_recorded_frame_texture);
 	glDeleteTextures(1, &msfb_color);
+
 	glDeleteRenderbuffers(1, &rbo);
 	glDeleteFramebuffers(1, &fbo);
 	glDeleteFramebuffers(1, &msfbo);
@@ -36,6 +37,7 @@ ComponentCamera::~ComponentCamera()
 void ComponentCamera::InitCamera()
 {
 	glGenFramebuffers(1, &fbo);
+	glGenFramebuffers(1, &msfbo);
 
 	aspect_ratio = 1.f;
 	camera_frustum.type = FrustumType::PerspectiveFrustum;
@@ -243,30 +245,29 @@ void ComponentCamera::CreateFramebuffer(float width, float height)
 
 void ComponentCamera::CreateMssaFramebuffer(float width, float height)
 {
-	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-
 	glGenTextures(1, &msfb_color);
 	glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, msfb_color);
 	glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, 4, GL_RGB, width, height, GL_TRUE);
 	glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, msfb_color, 0);
 
 	glGenRenderbuffers(1, &rbo);
 	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
 	glRenderbufferStorageMultisample(GL_RENDERBUFFER, 4, GL_DEPTH24_STENCIL8, width, height);
 	glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, msfb_color, 0);
 	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-	glGenFramebuffers(1, &msfbo);
-	glBindFramebuffer(GL_FRAMEBUFFER, msfbo);
 
 	glBindTexture(GL_TEXTURE_2D, last_recorded_frame_texture);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, last_recorded_frame_texture, 0);
+	glBindTexture(GL_TEXTURE_2D, 0);
 
+	glBindFramebuffer(GL_FRAMEBUFFER, msfbo);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, last_recorded_frame_texture, 0);
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
