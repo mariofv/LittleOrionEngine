093e0a49bb40a64c7fde88b17895b3b2938957d1
raulgonzalezupc <56253770+raulgonzalezupc@users.noreply.github.com>
2020-02-25 18:20:26 +0100

    Added directional and spot lights to phong shader

--- Game/Resources/Shaders/phong.fs
+++ Game/Resources/Shaders/phong.fs
@@ -25,8 +25,15 @@ layout (std140) uniform Matrices
 {
   mat4 model;
   mat4 proj;
-	mat4 view;
+  mat4 view;
 } matrices;
+
+layout (std140) uniform DirectionalLight
+{
+	vec3 color;
+	vec3 direction;
+	int num_directional_lights;
+} directional_light;
 
 struct PointLight
 {
@@ -37,6 +44,22 @@ struct PointLight
     float linear;
     float quadratic;
 };
+struct SpotLight 
+{
+	vec3 color;
+    vec3 position;
+    vec3 direction;
+    float cutOff;
+    float outerCutOff;
+  
+    float constant;
+    float linear;
+    float quadratic;     
+};
+
+
+uniform int num_spot_lights;
+uniform SpotLight spot_lights[10];
 
 uniform int num_point_lights;
 uniform PointLight point_lights[10];
@@ -47,6 +70,8 @@ vec4 GetSpecularColor(const Material mat, const vec2 texCoord);
 vec3 GetOcclusionColor(const Material mat, const vec2 texCoord);
 vec3 GetEmissiveColor(const Material mat, const vec2 texCoord);
 
+vec3 CalculateDirectionalLight(const vec3 normalized_normal);
+vec3 CalculateSpotLight(SpotLight spot_light, const vec3 normalized_normal);
 vec3 CalculatePointLight(PointLight point_light, const vec3 normalized_normal);
 
 void main()
@@ -54,12 +79,19 @@ void main()
 	vec3 normalized_normal = normalize(normal);
 
 	vec3 result = vec3(0);
-
+	for (int i = 0; i < directional_light.num_directional_lights; ++i)
+	{
+		result += CalculateDirectionalLight(normalized_normal);
+	}
 	for (int i = 0; i < num_point_lights; ++i)
 	{
 		result += CalculatePointLight(point_lights[i], normalized_normal);	
 	}
-
+	
+	for (int i = 0; i < num_spot_lights; ++i)
+	{
+		result += CalculateSpotLight(spot_lights[i], normalized_normal);
+	}
 	FragColor = vec4(result,1.0);
 }
 
@@ -84,6 +116,34 @@ vec3 GetEmissiveColor(const Material mat, const vec2 texCoord)
 }
 
 
+vec3 CalculateDirectionalLight(const vec3 normalized_normal)
+{
+
+	vec3 light_dir   = normalize(-directional_light.direction );
+	float diffuse    = max(0.0, dot(normalized_normal, light_dir));
+	float specular   = 0.0;
+
+	 vec3 view_pos    = transpose(mat3(matrices.proj))*(-matrices.proj[3].xyz);
+     vec3 view_dir    = normalize(view_pos-position);
+     vec3 reflect_dir = reflect(-light_dir, normalized_normal);
+     float spec       = max(dot(view_dir, reflect_dir), 0.0);
+
+	specular = pow(spec, material.shininess);
+	
+
+	vec4 diffuse_color  = GetDiffuseColor(material, texCoord);
+	vec4 specular_color  = GetSpecularColor(material, texCoord);
+	vec3 occlusion_color = GetOcclusionColor(material, texCoord);
+	vec3 emissive_color  = GetEmissiveColor(material, texCoord);
+
+
+	return directional_light.color * (
+		emissive_color
+		+ diffuse_color.rgb * (occlusion_color*material.k_ambient)
+		+ diffuse_color.rgb * material.k_diffuse * diffuse
+		+ specular_color.rgb * material.k_specular * specular
+	);
+}
 
 vec3 CalculatePointLight(PointLight point_light, const vec3 normalized_normal)
 {
@@ -117,5 +177,43 @@ vec3 CalculatePointLight(PointLight point_light, const vec3 normalized_normal)
 		+ diffuse_color.rgb *  material.k_diffuse * diffuse*attenuation
 		+ specular_color.rgb * material.k_specular * specular*attenuation
 	);
+
+}
+
+
+vec3 CalculateSpotLight(SpotLight spot_light, const vec3 normalized_normal)
+{
+	vec3 light_dir   = normalize(spot_light.position - position);
+    float diffuse    = max(0.0, dot(normalized_normal, light_dir));
+    float specular   = 0.0;
+
+	
+     vec3 view_pos    = transpose(mat3(matrices.proj))*(-matrices.proj[3].xyz);
+     vec3 view_dir    = normalize(view_pos-position);
+     vec3 reflect_dir = reflect(-light_dir, normalized_normal);
+     float spec       = max(dot(view_dir, reflect_dir), 0.0);
+
+     specular = pow(spec, material.shininess);
+      
+
+    vec4 diffuse_color  = GetDiffuseColor(material, texCoord);
+	vec4 specular_color  = GetSpecularColor(material, texCoord);
+	vec3 occlusion_color = GetOcclusionColor(material, texCoord);
+	vec3 emissive_color  = GetEmissiveColor(material, texCoord);
+
+    float theta = dot(light_dir, normalize(-spot_light.direction)); 
+    float epsilon = (spot_light.cutOff - spot_light.outerCutOff);
+    float intensity = clamp((theta - spot_light.outerCutOff) / epsilon, 0.0, 1.0);
+    
+    float distance    = length(spot_light.position - position);
+    float attenuation = 1.0 / (spot_light.constant + spot_light.linear * distance + 
+                spot_light.quadratic * (distance * distance));    
+
+   return spot_light.color * (
+        emissive_color
+        + diffuse_color.rgb * (occlusion_color*material.k_ambient)*attenuation
+        + diffuse_color.rgb * material.k_diffuse * diffuse*intensity*attenuation
+        + specular_color.rgb * material.k_specular * specular*intensity*attenuation
+    );
 
 }
