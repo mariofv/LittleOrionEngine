38624dc335c7e3d7dbd931205ae20e20806cbf27
JorxSS <49075678+JorxSS@users.noreply.github.com>
2020-02-27 21:32:39 +0100

    Moving GOs from Scripts

--- GamePlaySystem/src/Script/ExampleScript.cpp
+++ GamePlaySystem/src/Script/ExampleScript.cpp
@@ -1,7 +1,10 @@
 #include "ExampleScript.h"
 #include "Main/Application.h"
 #include "Module/ModuleInput.h"
+#include "Main/GameObject.h"
+#include "Component/ComponentTransform.h"
 #include "imgui.h"
+
 
 ExampleScript* ExampleScriptDLL()
 {
@@ -19,8 +22,11 @@ ExampleScript::~ExampleScript()
 
 void ExampleScript::Update()
 {
-	if (App->input->GetKeyDown(KeyCode::A)) {
+	float3 transform = this->owner->transform.GetTranslation();
+	
+	if (App->input->GetKey(KeyCode::A)) {
 		++i;
+		this->owner->transform.SetTranslation(float3(transform.x + speed,transform.y,transform.z));
 	}
 
 }
@@ -30,7 +36,7 @@ void ExampleScript::OnInspector(ImGuiContext* context)
 	ImGui::SetCurrentContext(context);
 	ImGui::Text("Testing Inspector");
 	ImGui::Text("By Marco and Jordi guys, we are doing it, yeah!");
-	ImGui::Text("%d", i);
+	ImGui::DragFloat("Speed:", &speed,0.01f,0.f,0.5f);
 	ImGui::Text("Se que es cutre Mario, pero es para testear");
 	
 }
