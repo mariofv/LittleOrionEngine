32b5ef77132e0effecd8f765886ed1b92052d4e0
marcorod94 <marco.rod.94@live.com>
2020-03-09 10:38:43 +0100

    Minor Fixes
    Use macros instead of absolute path, rename and delete .gitignore

---
+++ GameplaySystem/src/Script/ExampleScript.h
@@ -0,0 +1,18 @@
+#ifndef  __EXAMPLESCRIPT_H__
+#define  __EXAMPLESCRIPT_H__
+#include "Script.h"
+class ExampleScript : public Script
+{
+public:
+	ExampleScript();
+	~ExampleScript();
+	void Update() override;
+	void OnInspector(ImGuiContext*) override;
+	void Test();
+
+	int i = 0;
+	float speed = 0.01f;
+	float rotation_speed = 0.01f;
+};
+extern "C" SCRIPT_API ExampleScript* ExampleScriptDLL(); //This is how we are going to load the script
+#endif
