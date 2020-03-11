dd3da9a87c1ddffb5895472515dbbf4c84d33a9a
RickyVimon <33130090+RickyVimon@users.noreply.github.com>
2020-03-11 17:14:26 +0100

    basic method skelletons
    basic methods which need to be implemented

--- Engine/Animations/AnimControler.cpp
+++ Engine/Animations/AnimControler.cpp
@@ -1,12 +1,41 @@
 #include "AnimControler.h"
+#include "Main/Application.h"
+#include "Module/ModuleResourceManager.h"
 
 
 
 AnimControler::AnimControler()
 {
+	anim = App->resources->Load<Animation>("Library/Animations/2719853001_Take 001.anim").get();
 }
 
 
 AnimControler::~AnimControler()
 {
+}
+
+void AnimControler::Play(bool loop)
+{
+	if (loop)
+	{
+		Update();
+	}
+		Play(loop);
+}
+
+void AnimControler::Stop()
+{
+	loop = false;
+}
+
+void AnimControler::Update()
+{
+	if (loop) {
+
+	}
+}
+
+void AnimControler::GetTransform(char * name, float3 & pos, Quat & rot)
+{
+	//CurrentSample = (CurrentTime*(NumSamples - 1)) / AnimationDuration;
 }
--- Engine/Animations/AnimControler.h
+++ Engine/Animations/AnimControler.h
@@ -1,23 +1,31 @@
 #ifndef _ANIMCONTROLER_H_
 #define _ANIMCONTROLER_H_
 
+#include "ResourceManagement/Resources/Animation.h"
+
 class AnimControler
 {
 public:
 	AnimControler();
 	~AnimControler();
 
-	//uint CurrentTime;
-	bool Loop;
-	//resource UID;
+public:
+	bool loop;
+	Animation* anim;
+	unsigned int current_time = 0.f;
+	unsigned int animation_time = anim->duration;
+
+	float progress_animation = (current_time%animation_time / animation_time);
+
 
 public:
-	/*
-	Play(UID resource, bool loop);
-	Stop();
-	Update();
-	GetTransform(char* name, float3& pos, Quat& rot);
-	*/
+
+	void Play(bool loop);
+	void Stop();
+	void Update(); //TIMER???
+
+	void GetTransform(char* name, float3& pos, Quat& rot);
+	
 };
 
 #endif _ANIMCONTROLER_H_
