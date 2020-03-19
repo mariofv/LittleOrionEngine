75135134e47afa3caf506fff49a04d1913c711c9
JorxSS <49075678+JorxSS@users.noreply.github.com>
2020-03-19 12:04:05 +0100

    implementing logic for SceneCameraController

--- GameplaySystem/src/Script/SceneCamerasController.cpp
+++ GameplaySystem/src/Script/SceneCamerasController.cpp
@@ -2,6 +2,7 @@
 
 #include "Component/ComponentScript.h"
 #include "Component/ComponentTransform.h"
+#include "Component/ComponentCamera.h"
 
 #include "Main/Application.h"
 #include "Main/GameObject.h"
@@ -28,7 +29,11 @@ SceneCamerasController::SceneCamerasController()
 // Use this for initialization before Start()
 void SceneCamerasController::Awake()
 {
-
+	camera_list.push_back(main_camera);
+	camera_list.push_back(camera_1);
+	camera_list.push_back(camera_2);
+	camera_list.push_back(camera_3);
+	camera_rendering = (ComponentCamera*)main_camera->GetComponent(Component::ComponentType::CAMERA);
 }
 
 // Use this for initialization
@@ -40,7 +45,22 @@ void SceneCamerasController::Start()
 // Update is called once per frame
 void SceneCamerasController::Update()
 {
-
+	if (App->input->GetKeyDown(KeyCode::P)) 
+	{
+		if (index < camera_list.size()) 
+		{
+			++index;
+			UpdateCameraRendering();
+		}	
+	}
+	if (App->input->GetKeyDown(KeyCode::O))
+	{
+		if (index > 0)
+		{
+			--index;
+			UpdateCameraRendering();
+		}
+	}
 
 }
 
@@ -49,19 +69,64 @@ void SceneCamerasController::OnInspector(ImGuiContext* context)
 {
 	//Necessary to be able to write with imgui
 	ImGui::SetCurrentContext(context);
+	ImGui::Text("Game Camera: ");
+	ImGui::SameLine();
+	ImGui::Button(is_main_camera.c_str());
+	panel->DropGOTarget(main_camera);
+	if (main_camera)
+	{
+		is_main_camera = main_camera->name;
+	}
+	ImGui::Text("Camera 1: ");
+	ImGui::SameLine();
+	ImGui::Button(is_camera_1.c_str());
+	panel->DropGOTarget(camera_1);
+	if (camera_1)
+	{
+		is_camera_1 = camera_1->name;
+	}
+	ImGui::Text("Camera 2: ");
+	ImGui::SameLine();
+	ImGui::Button(is_camera_2.c_str());
+	panel->DropGOTarget(camera_2);
+	if (camera_2)
+	{
+		is_camera_2 = camera_2->name;
+	}
+	ImGui::Text("Camera 3: ");
+	ImGui::SameLine();
+	ImGui::Button(is_camera_3.c_str());
+	panel->DropGOTarget(camera_3);
+	if (camera_3)
+	{
+		is_camera_3 = camera_3->name;
+	}
 
+}
+
+void SceneCamerasController::UpdateCameraRendering()
+{
+	camera_rendering->Disable();
+	camera_rendering = (ComponentCamera*)camera_list[index]->GetComponent(Component::ComponentType::CAMERA);
+	camera_rendering->Enable();
 }
 
 //Use this for linking GO automatically
 void SceneCamerasController::Save(Config& config) const
 {
-	config.AddUInt(example->UUID, "ExampleNameforSave");
+	config.AddUInt(main_camera->UUID, "MainCamera");
+	config.AddUInt(camera_1->UUID, "Camera1");
+	config.AddUInt(camera_2->UUID, "Camera2");
+	config.AddUInt(camera_3->UUID, "Camera3");
 }
 
 //Use this for linking GO automatically
 void SceneCamerasController::Load(const Config& config)
 {
-	exampleUUID = config.GetUInt("ExampleNameforSave", 0);
+	maincameraUUID = config.GetUInt("MainCamera", 0);
+	camera1UUID = config.GetUInt("Camera1", 0);
+	camera2UUID = config.GetUInt("Camera2", 0);
+	camera2UUID = config.GetUInt("Camera3", 0);
 }
 
 //Use this for linking GO automatically
--- GameplaySystem/src/Script/SceneCamerasController.h
+++ GameplaySystem/src/Script/SceneCamerasController.h
@@ -2,6 +2,7 @@
 #define  __SCENECAMERASCONTROLLER_H__
 
 #include "Script.h"
+class ComponentCamera;
 
 class SceneCamerasController : public Script
 {
@@ -15,6 +16,8 @@ public:
 
 	void OnInspector(ImGuiContext*) override;
 
+	void UpdateCameraRendering();
+
 	void Save(Config& config) const override;
 	void Load(const Config& config) override;
 	void Link() override;
@@ -23,6 +26,26 @@ private:
 	PanelComponent* panel = nullptr;
 	GameObject* example = nullptr;
 	uint64_t exampleUUID = -1;
+
+	GameObject* main_camera = nullptr;
+	uint64_t maincameraUUID = -1;
+	std::string is_main_camera = "None";
+
+	GameObject* camera_1 = nullptr;
+	uint64_t camera1UUID = -1;
+	std::string is_camera_1 = "None";
+
+	GameObject* camera_2 = nullptr;
+	uint64_t camera2UUID = -1;
+	std::string is_camera_2 = "None";
+
+	GameObject* camera_3 = nullptr;
+	uint64_t camera3UUID = -1;
+	std::string is_camera_3 = "None";
+	
+	ComponentCamera* camera_rendering;
+	std::vector<GameObject*> camera_list;
+	int index = 0;
 
 };
 extern "C" SCRIPT_API SceneCamerasController* SceneCamerasControllerDLL(); //This is how we are going to load the script
