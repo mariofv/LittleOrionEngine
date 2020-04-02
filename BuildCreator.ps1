efc6a9040e0a3d9a25abb3ca4b26f5f256735189
Mario Fernández Villalba <mario_bcn2@hotmail.es>
2020-04-02 16:03:55 +0200

    Modify Buildcreator to create Engine and Game zips

--- BuildCreator.ps1
+++ BuildCreator.ps1
@@ -1,7 +1,16 @@
-Remove-Item -LiteralPath "Build" -Force -Recurse
-Remove-Item -LiteralPath "Build.zip" -Force -Recurse
+Remove-Item -LiteralPath "Engine" -Force -Recurse
+Remove-Item -LiteralPath "Engine.zip" -Force -Recurse
 
-Copy-Item -Path Game -Destination Build -Recurse -Force -Container
-Copy-Item Release\LittleOrionEngine.exe -Destination Build
+Remove-Item -LiteralPath "TwinlightSouls" -Force -Recurse
+Remove-Item -LiteralPath "TwinlightSouls.zip" -Force -Recurse.
 
-Compress-Archive -Path Build -DestinationPath Build.zip
+Copy-Item -Path Game -Destination Engine -Recurse -Force -Container
+Copy-Item Release\LittleOrionEngine.exe -Destination Engine
+
+Compress-Archive -Path Engine -DestinationPath Engine.zip
+
+Copy-Item -Path Game -Destination TwinlightSouls -Recurse -Force -Container
+Copy-Item TwinlightSouls\TwinlightSouls.exe -Destination TwinlightSouls
+
+Remove-Item -LiteralPath "TwinlightSouls\Assets" -Force -Recurse
+Compress-Archive -Path TwinlightSouls -DestinationPath TwinlightSouls.zip
