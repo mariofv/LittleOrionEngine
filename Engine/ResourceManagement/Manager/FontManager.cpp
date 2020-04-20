1a4f379e4076abaa5539124281b97bc9a9430cf1
Mario Fernández Villalba <mario_bcn2@hotmail.es>
2020-04-20 16:19:49 +0200

    Add FontImporter and FontManager

---
+++ Engine/ResourceManagement/Manager/FontManager.cpp
@@ -0,0 +1,13 @@
+#include "FontManager.h"
+
+#include "Main/Application.h"
+#include "Module/ModuleFileSystem.h"
+#include "Module/ModuleResourceManager.h"
+#include "ResourceManagement/Resources/Font.h"
+
+#include <Brofiler/Brofiler.h>
+
+std::shared_ptr<Font> FontManager::Load(uint32_t uuid, const FileData& resource_data)
+{
+	return nullptr;
+}
