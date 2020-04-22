b2b498d2c801289e25a4ba2dc6e04991d5c05a72
Enrique Alexandre González Sequeira <enrique.alexand.gonzalez@est.fib.upc.edu>
2020-04-22 22:58:26 +0200

    Create ModuleSpacePartitioning and move code from ModuleRender

--- Engine/Module/ModuleRender.cpp
+++ Engine/Module/ModuleRender.cpp
@@ -1,3 +1,5 @@
+#include "ModuleRender.h"
+
 #include "Main/Globals.h"
 #include "Main/Application.h"
 #include "ModuleCamera.h"
@@ -5,8 +7,8 @@
 #include "ModuleDebugDraw.h"
 #include "ModuleEditor.h"
 #include "ModuleProgram.h"
-#include "ModuleRender.h"
 #include "ModuleScene.h"
+#include "ModuleSpacePartitioning.h"
 #include "ModuleTime.h"
 #include "ModuleUI.h"
 #include "ModuleWindow.h"
@@ -204,149 +206,7 @@ void ModuleRender::GetMeshesToRender(const ComponentCamera *camera)
 	}
 	else
 	{
-		GetCullingMeshes(App->cameras->main_camera);
-	}
-}
-
-void ModuleRender::GetCullingMeshes(const ComponentCamera *camera)
-{
-	BROFILER_CATEGORY("Get culling meshes", Profiler::Color::Lavender);
-
-	meshes_to_render.clear();
-	switch (App->debug->culling_mode)
-	{
-	case ModuleDebug::CullingMode::NONE:
-		std::copy_if(
-			meshes.begin(),
-			meshes.end(),
-			std::back_inserter(meshes_to_render),
-			[camera](const auto& mesh)
-			{ 
-				return mesh->IsEnabled(); 
-			}
-		);
-		break;
-
-	case ModuleDebug::CullingMode::FRUSTUM_CULLING:
-		if (camera != nullptr)
-		{
-			std::copy_if(
-				meshes.begin(),
-				meshes.end(),
-				std::back_inserter(meshes_to_render),
-				[camera](const auto& mesh)
-			{
-				return mesh->IsEnabled() && mesh->owner->IsVisible(*camera);
-			}
-			);
-		}
-		break;
-
-	case ModuleDebug::CullingMode::QUADTREE_CULLING:
-		if (camera != nullptr)
-		{
-			// First we get all non static objects inside frustum
-			std::copy_if( 
-				meshes.begin(),
-				meshes.end(),
-				std::back_inserter(meshes_to_render),
-				[camera](const auto& mesh)
-			{
-				return mesh->IsEnabled() && mesh->owner->IsVisible(*camera) && !mesh->owner->IsStatic();
-			}
-			);
-
-			// Then we add all static objects culled using the quadtree
-			std::vector<GameObject*> rendered_objects;
-			ol_quadtree.CollectIntersect(rendered_objects, *camera);
-
-			for (const auto& object : rendered_objects)
-			{
-				ComponentMeshRenderer *object_mesh = (ComponentMeshRenderer*)object->GetComponent(Component::ComponentType::MESH_RENDERER);
-				meshes_to_render.push_back(object_mesh);
-			}
-		}
-		break;
-
-	case ModuleDebug::CullingMode::OCTTREE_CULLING:
-		if (camera != nullptr)
-		{
-			// First we get all non static objects inside frustum
-			std::copy_if(
-				meshes.begin(),
-				meshes.end(),
-				std::back_inserter(meshes_to_render),
-				[camera](const auto&  mesh)
-			{
-				return mesh->IsEnabled() && mesh->owner->IsVisible(*camera) && !mesh->owner->IsStatic();
-			}
-			);
-
-			// Then we add all static objects culled using the octtree
-			std::vector<GameObject*> rendered_objects;
-			ol_octtree.CollectIntersect(rendered_objects, *camera);
-
-			for (const auto& object: rendered_objects)
-			{
-				ComponentMeshRenderer *object_mesh = (ComponentMeshRenderer*)object->GetComponent(Component::ComponentType::MESH_RENDERER);
-				meshes_to_render.push_back(object_mesh);
-			}
-		}
-		break;
-
-	case ModuleDebug::CullingMode::AABBTREE_CULLING:
-		if (camera != nullptr)
-		{
-			// First we get all static objects inside frustum
-			std::copy_if(
-				meshes.begin(),
-				meshes.end(),
-				std::back_inserter(meshes_to_render),
-				[camera](const auto& mesh)
-			{
-				return mesh->IsEnabled() && mesh->owner->IsVisible(*camera) && mesh->owner->IsStatic();
-			}
-			);
-
-			// Then we add all dynamic objects culled using the aabbtree
-			std::vector<GameObject*> rendered_objects;
-			ol_abbtree->GetIntersection(rendered_objects, camera);
-
-			for (const auto& object : rendered_objects)
-			{
-				ComponentMeshRenderer *object_mesh = (ComponentMeshRenderer*)object->GetComponent(Component::ComponentType::MESH_RENDERER);
-				meshes_to_render.push_back(object_mesh);
-			}
-		}
-		break;
-	case ModuleDebug::CullingMode::COMBINED_CULLING:
-		if(camera != nullptr)
-		{
-			// We add all static objects culled using the quadtree
-			std::vector<GameObject*> rendered_static_objects;
-			ol_quadtree.CollectIntersect(rendered_static_objects, *camera);
-
-			for (const auto& object : rendered_static_objects)
-			{
-				ComponentMeshRenderer *object_mesh = (ComponentMeshRenderer*)object->GetComponent(Component::ComponentType::MESH_RENDERER);
-				meshes_to_render.push_back(object_mesh);
-			}
-
-			// Then we add all dynamic objects culled using the aabbtree
-			std::vector<GameObject*> rendered_dynamic_objects;
-			ol_abbtree->GetIntersection(rendered_dynamic_objects, camera);
-
-			for (const auto& object : rendered_dynamic_objects)
-			{
-				ComponentMeshRenderer *object_mesh = (ComponentMeshRenderer*)object->GetComponent(Component::ComponentType::MESH_RENDERER);
-				meshes_to_render.push_back(object_mesh);
-			}
-
-		}
-		break;
-
-	default:
-		break;
+		App->space_partitioning->GetCullingMeshes(App->cameras->main_camera);
 	}
 }
 
@@ -463,97 +323,11 @@ void ModuleRender::RemoveComponentMesh(ComponentMeshRenderer* mesh_to_remove)
 	}
 }
 
-void ModuleRender::GenerateQuadTree()
-{
-	AABB2D global_AABB;
-	global_AABB.SetNegativeInfinity();
-
-	for (const auto&  mesh : meshes)
-	{
-		float minX = std::fmin(mesh->owner->aabb.bounding_box2D.minPoint.x, global_AABB.minPoint.x);
-		float minY = std::fmin(mesh->owner->aabb.bounding_box2D.minPoint.y, global_AABB.minPoint.y);
-
-		float maxX = std::fmax(mesh->owner->aabb.bounding_box2D.maxPoint.x, global_AABB.maxPoint.x);
-		float maxY = std::fmax(mesh->owner->aabb.bounding_box2D.maxPoint.y, global_AABB.maxPoint.y);
-		global_AABB.maxPoint = float2(maxX,maxY);
-		global_AABB.minPoint = float2(minX, minY);
-
-	}
-
-	ol_quadtree.Create(global_AABB);
-	for (const auto&  mesh : meshes)
-	{
-		ol_quadtree.Insert(*mesh->owner);
-	}
-}
-
-void ModuleRender::GenerateOctTree()
-{
-	AABB global_AABB;
-	global_AABB.SetNegativeInfinity();
-
-	for (const auto&  mesh : meshes)
-	{
-		float minX = std::fmin(mesh->owner->aabb.bounding_box.minPoint.x, global_AABB.minPoint.x);
-		float minY = std::fmin(mesh->owner->aabb.bounding_box.minPoint.y, global_AABB.minPoint.y);
-		float minZ = std::fmin(mesh->owner->aabb.bounding_box.minPoint.z, global_AABB.minPoint.z);
-
-		float maxX = std::fmax(mesh->owner->aabb.bounding_box.maxPoint.x, global_AABB.maxPoint.x);
-		float maxY = std::fmax(mesh->owner->aabb.bounding_box.maxPoint.y, global_AABB.maxPoint.y);
-		float maxZ = std::fmax(mesh->owner->aabb.bounding_box.maxPoint.z, global_AABB.maxPoint.z);
-
-		global_AABB.maxPoint = float3(maxX, maxY, maxZ);
-		global_AABB.minPoint = float3(minX, minY, minZ);
-	}
-
-	/*ol_octtree.Create(global_AABB);
-	for (const auto&  mesh : meshes)
-	{
-		ol_octtree.Insert(*mesh->owner);
-	}*/
-}
-void ModuleRender::InsertAABBTree(GameObject * game_object)
-{
-	ComponentMeshRenderer* object_mesh = (ComponentMeshRenderer*)game_object->GetComponent(Component::ComponentType::MESH_RENDERER);
-	if(object_mesh != nullptr)
-		ol_abbtree->Insert(game_object);
-}
-
-void ModuleRender::RemoveAABBTree(GameObject * game_object)
-{
-	ComponentMeshRenderer* object_mesh = (ComponentMeshRenderer*)game_object->GetComponent(Component::ComponentType::MESH_RENDERER);
-	if (object_mesh != nullptr)
-		ol_abbtree->Remove(game_object);
-}
-
-void ModuleRender::UpdateAABBTree(GameObject* game_object)
-{
-	ComponentMeshRenderer* object_mesh = (ComponentMeshRenderer*)game_object->GetComponent(Component::ComponentType::MESH_RENDERER);
-	if (object_mesh != nullptr)
-		ol_abbtree->UpdateObject(game_object);
-}
-
-void ModuleRender::DeleteAABBTree()
-{
-	delete ol_abbtree;
-}
-
-void ModuleRender::CreateAABBTree()
-{
-	ol_abbtree = new OLAABBTree(INITIAL_SIZE_AABBTREE);
-}
-
-void ModuleRender::DrawAABBTree() const
-{
-	BROFILER_CATEGORY("Render AABBTree", Profiler::Color::Lavender);
-
-	ol_abbtree->Draw();
-}
 
 GameObject* ModuleRender::GetRaycastIntertectedObject(const LineSegment& ray)
 {
 	BROFILER_CATEGORY("Do Raycast", Profiler::Color::HotPink);
-	GetCullingMeshes(App->cameras->scene_camera);
+	App->space_partitioning->GetCullingMeshes(App->cameras->scene_camera);
 	std::vector<ComponentMeshRenderer*> intersected_meshes;
 	for (const auto&  mesh : meshes_to_render)
 	{
@@ -595,7 +369,7 @@ GameObject* ModuleRender::GetRaycastIntertectedObject(const LineSegment& ray)
 
 bool ModuleRender::GetRaycastIntertectedObject(const LineSegment& ray, float3& position)
 {
-	GetCullingMeshes(App->cameras->scene_camera);
+	App->space_partitioning->GetCullingMeshes(App->cameras->scene_camera);
 	std::vector<ComponentMeshRenderer*> intersected_meshes;
 	for (const auto&  mesh : meshes_to_render)
 	{
---
+++ Engine/Module/ModuleSpacePartitioning.h
@@ -0,0 +1,46 @@
+#ifndef _MODULESPACEPARTITIONING_H_
+#define _MODULESPACEPARTITIONING_H_
+
+#include "Module.h"
+#include <memory>
+
+class OLAABBTree;
+class OLOctTree;
+class OLQuadTree;
+
+class ComponentCamera;
+class GameObject;
+
+const unsigned INITIAL_SIZE_AABBTREE = 10;
+
+class ModuleSpacePartitioning : public Module
+{
+public:
+	ModuleSpacePartitioning() = default;
+	~ModuleSpacePartitioning() = default;
+
+	bool Init();
+	update_status PreUpdate();
+	bool CleanUp();
+
+	void GenerateQuadTree();
+	void GenerateOctTree();
+	void InsertAABBTree(GameObject* game_object);
+	void RemoveAABBTree(GameObject* game_object);
+	void UpdateAABBTree(GameObject* game_object);
+	void ResetAABBTree();
+	void DrawAABBTree() const;
+	void GetCullingMeshes(const ComponentCamera* camera);
+
+private:
+	std::unique_ptr<OLQuadTree> ol_quadtree = nullptr;
+	std::unique_ptr<OLOctTree> ol_octtree = nullptr;
+	std::unique_ptr<OLAABBTree> ol_abbtree = nullptr;
+
+	friend class ModuleDebugDraw;
+	friend class PanelConfiguration;
+	friend class PanelDebug;
+	friend class PanelScene;
+};
+
+#endif //_MODULESPACEPARTITIONING_H_
