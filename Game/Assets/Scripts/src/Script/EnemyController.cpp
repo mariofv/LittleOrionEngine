e958f01a5ffce9dbd8ffb1baa8582d231afb8f21
JoanStinson <jgm7.hd@gmail.com>
2020-05-06 18:57:17 +0200

    Starting Enemy Controller class

--- Game/Assets/Scripts/src/Script/EnemyController.cpp
+++ Game/Assets/Scripts/src/Script/EnemyController.cpp
@@ -1,71 +1,73 @@
-#include "EnemyController.h"
+#include "EnemyController.h"
 
 #include "Component/ComponentScript.h"
 #include "Component/ComponentTransform.h"
 #include "Main/Application.h"
 #include "Main/GameObject.h"
 #include "Module/ModuleInput.h"
-#include "Module/ModuleScene.h"
+#include "Module/ModuleScene.h"
 
-#include "EditorUI/Panel/InspectorSubpanel/PanelComponent.h"
-
-#include "imgui.h"
-
+#include "EditorUI/Panel/InspectorSubpanel/PanelComponent.h"
+
+#include "imgui.h"
+
 EnemyController* EnemyControllerDLL()
 {
 	EnemyController* instance = new EnemyController();
 	return instance;
-}
+}
 
 EnemyController::EnemyController()
 {
 	panel = new PanelComponent();
 }
 
-
 // Use this for initialization before Start()
 void EnemyController::Awake()
 {
 	EnemyManager::GetInstance()->AddEnemy(this);
-}
+}
 
 // Use this for initialization
 void EnemyController::Start()
-{
+{
 
-}
-
+}
+
 // Update is called once per frame
 void EnemyController::Update()
-{
-}
-
+{
+
+}
+
 // Use this for showing variables on inspector
 void EnemyController::OnInspector(ImGuiContext* context)
 {
 	//Necessary to be able to write with imgui
 	ImGui::SetCurrentContext(context);
 	ShowDraggedObjects();
-}
-
+}
+
 //Use this for linking JUST GO automatically 
 void EnemyController::InitPublicGameObjects()
 {
 	//IMPORTANT, public gameobjects, name_gameobjects and go_uuids MUST have same size
 	public_gameobjects.push_back(&player);
-	variable_names.push_back(GET_VARIABLE_NAME(player));
+	variable_names.push_back(GET_VARIABLE_NAME(player));
+
 	for (int i = 0; i < public_gameobjects.size(); ++i)
 	{
 		name_gameobjects.push_back(is_object);
 		go_uuids.push_back(0);
-	}
+	}
+
 }
 
 void EnemyController::TakeDamage(float damage)
 {
 	health_points -= damage;
 
-	if(health_points <= 0.0f)
+	if(health_points <= 0)
 	{
 		Death();
 		//F
@@ -74,7 +76,5 @@ void EnemyController::TakeDamage(float damage)
 
 void EnemyController::Death()
 {
-
-}
-
-
+	//TODO spawn particles, loot, etc.
+}
--- Game/Assets/Scripts/src/Script/EnemyController.h
+++ Game/Assets/Scripts/src/Script/EnemyController.h
@@ -1,30 +1,56 @@
 #ifndef  __ENEMYCONTROLLER_H__
-#define  __ENEMYCONTROLLER_H__
+#define  __ENEMYCONTROLLER_H__
+
 #include "Script.h"
 
 #include "EnemyManager.h"
 
+class ComponentAnimation;
+
+enum class EnemyType
+{
+	MUSHDOOM
+};
+
 class EnemyController : public Script
-{
-public:
+{
+public:
 	EnemyController();
-	~EnemyController() = default;
-
+	~EnemyController() = default;
+
 	void Awake() override;
 	void Start() override;
-	void Update() override;
-
+	void Update() override;
+
 	void OnInspector(ImGuiContext*) override;
-	void InitPublicGameObjects();	void TakeDamage(float damage);
+	void InitPublicGameObjects();
+	void TakeDamage(float damage);
 
 	EnemyManager* punterito = nullptr;
 
-private:
+protected:
+	void Move();
+	bool Attack();
+	bool PlayerInSight();
+	void OnCollisionEnter();
 	void Death();
-
-private:
-	GameObject* player = nullptr;	float health_points = 100.0f;
-
-};
-extern "C" SCRIPT_API EnemyController* EnemyControllerDLL(); //This is how we are going to load the script
+
+protected:
+	EnemyType type;
+	GameObject* player = nullptr;
+	ComponentAnimation* animation = nullptr;
+
+	float move_speed;
+	float attack_speed;
+	float attack_power = 50.f;
+	float attack_range;
+	float health_points = 100.f;
+	float detected_player_distance;
+
+	bool is_dead = false;
+	bool is_attacking = false;
+};
+
+extern "C" SCRIPT_API EnemyController* EnemyControllerDLL(); //This is how we are going to load the script
+
 #endif
