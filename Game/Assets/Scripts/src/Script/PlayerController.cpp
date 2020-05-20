94cb4dd7024e4da418a41673ade93199e304ce55
Enrique Alexandre González Sequeira <enrique.alexand.gonzalez@est.fib.upc.edu>
2020-05-20 14:01:47 +0200

    Control if player is dead while hp is under 0

--- Game/Assets/Scripts/src/Script/PlayerController.cpp
+++ Game/Assets/Scripts/src/Script/PlayerController.cpp
@@ -52,6 +52,11 @@ void PlayerController::Update()
 	{
 		player_movement->Move(player);
 	}
+
+	if(health_points <= 0)
+	{
+		is_alive = false;
+	}
 }
 
 // Use this for showing variables on inspector
