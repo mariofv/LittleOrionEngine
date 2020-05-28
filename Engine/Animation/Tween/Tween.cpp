df93f498be47659c5ff81d3c6d108ecdbc9836b4
Luis <luis@luismoyano.net>
2020-05-28 23:08:25 +0200

    A few missing methods

--- Engine/Animation/Tween/Tween.cpp
+++ Engine/Animation/Tween/Tween.cpp
@@ -1,5 +1,19 @@
 #include "Tween.h"
 
+void Tween::Play()
+{
+	if (state == TweenState::COMPLETED) return;
+
+	state = TweenState::PLAYING;
+}
+
+void Tween::Pause()
+{
+	if (state != TweenState::PLAYING) return;
+
+	state = TweenState::PAUSED;
+}
+
 void Tween::Update(float dt)
 {
 	if (state == TweenState::COMPLETED || state == TweenState::PAUSED) return;
