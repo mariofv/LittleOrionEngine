63b1fd421976f949b08410da9d8433c8447d8ee9
raulgonzalezupc <56253770+raulgonzalezupc@users.noreply.github.com>
2020-06-11 17:25:40 +0200

    Group normal map and liquid map computations in a method

--- Game/Resources/Shaders/blinn_phong.fs
+++ Game/Resources/Shaders/blinn_phong.fs
@@ -126,7 +126,7 @@ vec3 CalculateLightmap(const vec3 normalized_normal, vec4 diffuse_color, vec4 sp
 //COMPUTE NORMALIZED LIGHTS
 vec3 ComputeDiffuseColor(vec3 diffuse_color, vec3 specular_color);
 float ComputeSpecularLight(vec3 normal, vec3 half_dir);
-
+vec3 CalculateNormalMapAndLiquid(const Material material, const vec2 texCoord);
 
 void main()
 {
@@ -144,36 +144,7 @@ void main()
 	vec3 emissive_color  = GetEmissiveColor(material, tiling);
 
 	vec3 fragment_normal = normal;
-	
-	if(material.use_liquid_map && !material.use_normal_map )
-	{
-		vec2 tiling_nm_x = vec2(material.tiling_liquid_x_x, material.tiling_liquid_x_y)+tiling;
-		vec2 tiling_nm_y = vec2(material.tiling_liquid_y_x, material.tiling_liquid_y_y)+tiling;
-		vec3 normal_texture_x = GetLiquidMap(material, tiling_nm_x);
-		vec3 normal_texture_y = GetLiquidMap(material, tiling_nm_y);
-		liquid_normal_from_texture =  mix(normal_texture_x, normal_texture_y, 0.5);
-		fragment_normal = normalize(TBN * liquid_normal_from_texture);
-	}
-	else if(material.use_normal_map && !material.use_liquid_map)
-	{
-		normal_from_texture = GetNormalMap(material, tiling);
-		fragment_normal = normalize(TBN * normal_from_texture);
-	}
-	else if(material.use_normal_map && material.use_liquid_map)
-	{
-		vec2 tiling_nm_x = vec2(material.tiling_liquid_x_x, material.tiling_liquid_x_y)+tiling;
-		vec2 tiling_nm_y = vec2(material.tiling_liquid_y_x, material.tiling_liquid_y_y)+tiling;
-		vec3 normal_texture_x = GetLiquidMap(material, tiling_nm_x);
-		vec3 normal_texture_y = GetLiquidMap(material, tiling_nm_y);
-		liquid_normal_from_texture =  mix(normal_texture_x, normal_texture_y, 0.5);
-		normal_from_texture = GetNormalMap(material, tiling);
-		vec3 final_normal_map = mix(liquid_normal_from_texture, normal_from_texture, 0.5);
-		fragment_normal = normalize(TBN * final_normal_map);
-	}
-
-			
-	
-	
+	fragment_normal = CalculateNormalMapAndLiquid(material, tiling);
 	result += CalculateLightmap(fragment_normal, diffuse_color,  specular_color, occlusion_color,  emissive_color);
 	for (int i = 0; i < directional_light.num_directional_lights; ++i)
 	{
@@ -239,7 +210,37 @@ vec3 GetLiquidMap(const Material mat, const vec2 texCoord)
 {
 	return texture(mat.liquid_map, texCoord).rgb*2.0-1.0;
 }
+vec3 CalculateNormalMapAndLiquid(const Material material, const vec2 tiling)
+{
+	vec3 result;
+	if(material.use_liquid_map && !material.use_normal_map )
+	{
+		vec2 tiling_nm_x = vec2(material.tiling_liquid_x_x, material.tiling_liquid_x_y)+tiling;
+		vec2 tiling_nm_y = vec2(material.tiling_liquid_y_x, material.tiling_liquid_y_y)+tiling;
+		vec3 normal_texture_x = GetLiquidMap(material, tiling_nm_x);
+		vec3 normal_texture_y = GetLiquidMap(material, tiling_nm_y);
+		liquid_normal_from_texture =  mix(normal_texture_x, normal_texture_y, 0.5);
+		result = normalize(TBN * liquid_normal_from_texture);
+	}
+	else if(material.use_normal_map && !material.use_liquid_map)
+	{
+		normal_from_texture = GetNormalMap(material, tiling);
+		result = normalize(TBN * normal_from_texture);
+	}
+	else if(material.use_normal_map && material.use_liquid_map)
+	{
+		vec2 tiling_nm_x = vec2(material.tiling_liquid_x_x, material.tiling_liquid_x_y)+tiling;
+		vec2 tiling_nm_y = vec2(material.tiling_liquid_y_x, material.tiling_liquid_y_y)+tiling;
+		vec3 normal_texture_x = GetLiquidMap(material, tiling_nm_x);
+		vec3 normal_texture_y = GetLiquidMap(material, tiling_nm_y);
+		liquid_normal_from_texture =  mix(normal_texture_x, normal_texture_y, 0.5);
+		normal_from_texture = GetNormalMap(material, tiling);
+		vec3 final_normal_map = mix(liquid_normal_from_texture, normal_from_texture, 0.5);
+		result=normalize(TBN * final_normal_map);
+	}
+	return result;
 
+}
 vec3 CalculateDirectionalLight(const vec3 normalized_normal, vec4 diffuse_color, vec4 specular_color, vec3 occlusion_color, vec3 emissive_color)
 {
 
