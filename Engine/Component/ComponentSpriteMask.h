350eeb8be01987adb6d6a3d8b029a904cfcedfbc
Mario Fernández Villalba <mario_bcn2@hotmail.es>
2020-06-14 23:38:23 +0200

    Add new Component, ComponentSpriteMask

---
+++ Engine/Component/ComponentSpriteMask.h
@@ -0,0 +1,44 @@
+#ifndef _COMPONENTSPRITEMASK_H_
+#define _COMPONENTSPRITEMASK_H_
+
+#include "Component/Component.h"
+
+#include <GL/glew.h>
+
+class Texture;
+
+class ComponentSpriteMask : public Component
+{
+public:
+	ComponentSpriteMask();
+	ComponentSpriteMask(GameObject * owner);
+	~ComponentSpriteMask();
+
+	Component* Clone(bool original_prefab = false) const override;
+	void Copy(Component* component_to_copy) const override;
+
+	void Delete() override;
+	void SpecializedSave(Config& config) const override;
+	void SpecializedLoad(const Config& config) override;
+
+	void SetTextureToRender(uint32_t texture_uuid);
+
+	void Render(float4x4* projection);
+
+	void SetNativeSize() const;
+
+private:
+	virtual void InitData();
+
+public:
+	uint32_t texture_uuid = 0;
+	std::shared_ptr<Texture> texture_to_render;
+	bool preserve_aspect_ratio = false;
+
+
+private:
+	GLuint program, vao, vbo;
+
+	float texture_aspect_ratio = 0.f;
+};
+#endif //_COMPONENTSPRITEMASK_H_
