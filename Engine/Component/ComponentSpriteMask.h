bba207e1a7ab26169d16f1f38507533a7066acbd
Mario Fernández Villalba <mario_bcn2@hotmail.es>
2020-06-15 02:36:58 +0200

    Refactor how UI renders, SpriteMask is applied to all children

--- Engine/Component/ComponentSpriteMask.h
+++ Engine/Component/ComponentSpriteMask.h
@@ -12,7 +12,7 @@ class ComponentSpriteMask : public Component
 public:
 	ComponentSpriteMask();
 	ComponentSpriteMask(GameObject * owner);
-	~ComponentSpriteMask();
+	~ComponentSpriteMask() = default;
 
 	Component* Clone(bool original_prefab = false) const override;
 	void Copy(Component* component_to_copy) const override;
@@ -21,25 +21,8 @@ public:
 	void SpecializedSave(Config& config) const override;
 	void SpecializedLoad(const Config& config) override;
 
-	void SetTextureToRender(uint32_t texture_uuid);
-
-	void Render(float4x4* projection);
-
-	void SetNativeSize();
-
-private:
-	virtual void InitData();
-
 public:
-	uint32_t texture_uuid = 0;
-	std::shared_ptr<Texture> texture_to_render;
-	float2 size;
 	bool render_mask = false;
 	bool inverted_mask = false;
-
-private:
-	GLuint program, vao, vbo;
-
-	float texture_aspect_ratio = 0.f;
 };
 #endif //_COMPONENTSPRITEMASK_H_
