a039d95be9479272c195bdb369896bb3394d2a3a
Mario Fernández Villalba <mario_bcn2@hotmail.es>
2020-06-16 12:18:11 +0200

    Make camera frustum public

--- Engine/Component/ComponentCamera.h
+++ Engine/Component/ComponentCamera.h
@@ -141,13 +141,12 @@ public:
 	bool toggle_msaa = false;
 	bool is_focusing = false;
 
+	Frustum camera_frustum;
+
 private:
 	Frustum camera_frustum;
 	GLuint rbo = 0;
 	GLuint fbo = 0;
-private:
-	
-	
 	
 	GLuint msfbo = 0;
 	GLuint msfb_color = 0;
