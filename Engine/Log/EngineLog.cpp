bc24257f117a707992c31646e2f0d129e7637a0b
Mario Fernández Villalba <mario_bcn2@hotmail.es>
2020-07-07 19:55:49 +0200

    Removed Windows console

--- Engine/Log/EngineLog.cpp
+++ Engine/Log/EngineLog.cpp
@@ -12,49 +12,42 @@
 EngineLog::EngineLog()
 {
 	auto visual_studio_debug_sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
-	auto windows_console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 	auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/log.txt", true);
 
 	auto little_orion_duplicate_filter = std::make_shared<spdlog::sinks::dup_filter_sink_st>(std::chrono::seconds(5));
 	little_orion_sink = std::make_shared<engine_sink_mt>();
 	little_orion_duplicate_filter->add_sink(little_orion_sink);
 	little_orion_duplicate_filter->add_sink(visual_studio_debug_sink);
-	little_orion_duplicate_filter->add_sink(windows_console_sink);
 	little_orion_duplicate_filter->add_sink(file_sink);
 
 	auto game_duplicate_filter = std::make_shared<spdlog::sinks::dup_filter_sink_st>(std::chrono::seconds(5));
 	game_sink = std::make_shared<engine_sink_mt>();
 	game_duplicate_filter->add_sink(game_sink);
 	game_duplicate_filter->add_sink(visual_studio_debug_sink);
-	game_duplicate_filter->add_sink(windows_console_sink);
 	game_duplicate_filter->add_sink(file_sink);
 
 	auto opengl_duplicate_filter = std::make_shared<spdlog::sinks::dup_filter_sink_st>(std::chrono::seconds(5));
 	opengl_sink = std::make_shared<engine_sink_mt>();
 	opengl_duplicate_filter->add_sink(opengl_sink);
 	opengl_duplicate_filter->add_sink(visual_studio_debug_sink);
-	opengl_duplicate_filter->add_sink(windows_console_sink);
 	opengl_duplicate_filter->add_sink(file_sink);
 
 	auto assimp_duplicate_filter = std::make_shared<spdlog::sinks::dup_filter_sink_st>(std::chrono::seconds(5));
 	assimp_sink = std::make_shared<engine_sink_mt>();
 	assimp_duplicate_filter->add_sink(assimp_sink);
 	assimp_duplicate_filter->add_sink(visual_studio_debug_sink);
-	assimp_duplicate_filter->add_sink(windows_console_sink);
 	assimp_duplicate_filter->add_sink(file_sink);
 
 	auto resources_duplicate_filter = std::make_shared<spdlog::sinks::dup_filter_sink_st>(std::chrono::seconds(5));
 	resources_sink = std::make_shared<engine_sink_mt>();
 	resources_duplicate_filter->add_sink(resources_sink);
 	resources_duplicate_filter->add_sink(visual_studio_debug_sink);
-	resources_duplicate_filter->add_sink(windows_console_sink);
 	resources_duplicate_filter->add_sink(file_sink);
 
 	auto debug_duplicate_filter = std::make_shared<spdlog::sinks::dup_filter_sink_st>(std::chrono::seconds(5));
 	debug_sink = std::make_shared<engine_sink_mt>();
 	debug_duplicate_filter->add_sink(debug_sink);
 	debug_duplicate_filter->add_sink(visual_studio_debug_sink);
-	debug_duplicate_filter->add_sink(windows_console_sink);
 	debug_duplicate_filter->add_sink(file_sink);
 
 	little_orion_logger = std::make_shared<spdlog::logger>("LittleOrionEngine", little_orion_duplicate_filter);
