ddfbbefc373fbb0a63e1819d9cb4a4cab2d2ed22
Enrique Alexandre González Sequeira <enrique.alexand.gonzalez@est.fib.upc.edu>
2020-07-13 19:21:18 +0200

    Only update multithreaded data if MULTITHREADING

--- Engine/ResourceManagement/Resources/Scene.cpp
+++ Engine/ResourceManagement/Resources/Scene.cpp
@@ -102,10 +102,13 @@ void Scene::Load(bool from_file)
 	}
 
 	timer.Start();
+
+#if MULTITHREADING
 	App->resources->loading_thread_communication.number_of_textures_loaded = 0;
 	App->resources->loading_thread_communication.current_number_of_resources_loaded = 0;
 	App->resources->loading_thread_communication.total_number_of_resources_to_load = 0;
 	App->resources->loading_thread_communication.loading = true;
+#endif
 
 	std::unordered_map<int64_t, std::vector<GameObject*>> prefab_parents;
 	std::vector<Config> prefabs_config;
