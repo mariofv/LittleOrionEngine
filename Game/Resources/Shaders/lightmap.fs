243103ee7ef8c5f750cf0b78867d911b70207756
yalania <anaisabel14hb@gmail.com>
2020-07-14 18:16:37 +0200

    fix lightmap shader

--- Game/Resources/Shaders/lightmap.fs
+++ Game/Resources/Shaders/lightmap.fs
@@ -16,7 +16,7 @@ in mat3 TBN;
 out vec4 FragColor;
 
 //constants
-const float gamma = 2.2;
+
 const float ambient_light_strength = 0.1;
 
 vec3 normal_from_texture;
@@ -62,14 +62,10 @@ layout (std140) uniform Matrices
 //COLOR TEXTURES
 vec4 GetDiffuseColor(const Material mat, const vec2 texCoord);
 vec4 GetSpecularColor(const Material mat, const vec2 texCoord);
-vec3 CalculateLightmap(const vec3 normalized_normal, vec4 diffuse_color, vec4 specular_color, vec3 occlusion_color, vec3 emissive_color);
 vec3 GetOcclusionColor(const Material mat, const vec2 texCoord);
 vec3 GetEmissiveColor(const Material mat, const vec2 texCoord);
 
 //MAPS
-vec3 GetNormalMap(const Material mat, const vec2 texCoord);
-
-vec3 NormalizedDiffuse(vec3 diffuse_color, vec3 frensel);
 
 uniform float ambient_light_intensity;
 uniform vec4 ambient_light_color;
@@ -87,24 +83,11 @@ void main()
 	vec3 occlusion_color = GetOcclusionColor(material, tiling);
 	vec3 emissive_color  = GetEmissiveColor(material, tiling);
 
-	vec3 fragment_normal = normalize(normal);
 
-	//Tangent space matrix
-	vec3 T = normalize(vec3(matrices.model * vec4(vertex_tangent_fs,   0.0)));
-	vec3 N = normalize(vec3(matrices.model * vec4(vertex_normal_fs,    0.0)));
-	vec3 ortho_tangent = normalize(T-dot(T, N)*N); // Gram-Schmidt
-	vec3 B = normalize(cross(N, ortho_tangent));
-	mat3 TBN = mat3(T, B, N);
-
-	#if NORMAL_MAP
-		vec3 normal_from_texture = GetNormalMap(material, tiling);
-		fragment_normal= normalize(TBN * normal_from_texture);
-	#endif
-	result += CalculateLightmap(fragment_normal, diffuse_color,  specular_color, occlusion_color,  emissive_color);
+	result += texture(material.light_map, texCoordLightmap).rgb;
 	result += emissive_color;
 	result += diffuse_color.rgb * ambient * occlusion_color.rgb; //Ambient light
 	FragColor = vec4(result,1.0);
-	FragColor.rgb = pow(FragColor.rgb, vec3(1/gamma)); //Gamma Correction - The last operation of postprocess
 	FragColor.a=material.transparency;
 }
 
@@ -138,31 +121,6 @@ vec3 GetEmissiveColor(const Material mat, const vec2 texCoord)
 	return (texture(mat.emissive_map, texCoord)*mat.emissive_color).rgb;
 }
 
-vec3 GetNormalMap(const Material mat, const vec2 texCoord)
-{
-	return normalize(texture(mat.normal_map, texCoord).rgb*2.0-1.0);
-}
 
-vec3 NormalizedDiffuse(vec3 diffuse_color, vec3 frensel)
-{
-	return (1-frensel)*diffuse_color/PI;
-}
 
-vec3 CalculateLightmap(const vec3 normalized_normal, vec4 diffuse_color, vec4 specular_color, vec3 occlusion_color, vec3 emissive_color)
-{
-
-		vec3 light_dir   =vec3(1.0f,1.0f,1.0f);
-	vec3 half_dir 	 = normalize(light_dir + view_dir);
-	float ND = max(0.0, dot(normalized_normal, light_dir));
-
-	//----Specular calculations----
-	float shininess = 7*specular_color.a + 1;
-	shininess *= shininess;
-	float spec = pow(max(dot(normalized_normal, half_dir), 0.0), shininess);
-	vec3 fresnel =  specular_color.rgb + (1-specular_color.rgb)* pow((1.0-ND),5);
-	vec3 specular =(spec * (shininess+8)/8*PI) *  fresnel;
-
-	return texture(material.light_map, texCoord).rgb * (NormalizedDiffuse(diffuse_color.rgb, fresnel) + specular)* ND;
-
-}
 
