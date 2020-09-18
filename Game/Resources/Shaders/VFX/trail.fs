e4d8eff94faede3a47e4a7a0ecadb40d459316f2
Mario Fernández Villalba <mario_bcn2@hotmail.es>
2020-09-18 12:30:36 +0200

    Reorder shaders folder

---
+++ Game/Resources/Shaders/VFX/trail.fs
@@ -0,0 +1,21 @@
+in vec2 texCoord;
+
+layout (location = 0) out vec4 FragColor;
+layout (location = 1) out vec4 BrightColor;
+
+uniform sampler2D tex;
+uniform vec4 color;
+uniform float emisive_exposure;
+
+
+void main()
+{
+	vec4 texture_color = texture(tex, texCoord) * color * emisive_exposure;
+	FragColor =  texture_color;
+	float brightness = dot(texture_color.rgb, vec3(0.2126, 0.7152, 0.0722));
+  if(brightness > 1.0)
+      BrightColor = vec4(texture_color.rgb, 1.0);
+  else
+      BrightColor = vec4(0.0, 0.0, 0.0, 1.0);
+
+}
