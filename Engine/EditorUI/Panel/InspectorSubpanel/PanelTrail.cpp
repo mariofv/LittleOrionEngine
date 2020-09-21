e4c34102e66bbc5c8025273d67a58a4b8c23434a
Misarte <art.geo14@gmail.com>
2020-09-21 14:53:03 +0200

    Merge remote-tracking branch 'origin/develop' into feature/trails
    Changed Width conf speed to 0.01f

--- Engine/EditorUI/Panel/InspectorSubpanel/PanelTrail.cpp
+++ Engine/EditorUI/Panel/InspectorSubpanel/PanelTrail.cpp
@@ -45,7 +45,7 @@ void PanelTrail::Render(ComponentTrail* trail)
 		ImGui::Separator();
 		ImGui::TextColored(ImVec4(0.f, 0.f, 1.f, 1.f), "Trail Properties");
 		trail->modified_by_user |= ImGui::DragFloat("Time", &trail->duration, 1000.0f);
-		trail->modified_by_user |= ImGui::DragFloat("Width", &trail->width, 0.1f);
+		trail->modified_by_user |= ImGui::DragFloat("Width", &trail->width, 0.01f);
 		trail->modified_by_user |= ImGui::DragInt("Curve Segments", &trail->points_in_curve, 1, 0, 100);
 		
 		float trail_texture_size = 17.5f;
@@ -110,21 +110,6 @@ void PanelTrail::Render(ComponentTrail* trail)
 			trail->modified_by_user |= ImGui::ColorEdit4("Color", trail->color.ptr());
 			trail->modified_by_user |= ImGui::DragFloat("Intensity", &trail->bloom_intensity, 0.05f, 0.01f, 10.0f);
 
-			ImGui::Text("Fade Between Colors");
-			trail->modified_by_user |= ImGui::Checkbox("###Fade Between Colors", &trail->fade_between_colors);
-			ImGui::SameLine();
-			if (!trail->fade_between_colors)
-			{
-				ImGui::PushItemFlag(ImGuiItemFlags_Disabled, true);
-				ImGui::PushStyleVar(ImGuiStyleVar_Alpha, ImGui::GetStyle().Alpha * 0.5f);
-			}
-			trail->modified_by_user |= ImGui::ColorEdit4("Color To Fade##2f", trail->color_to_fade.ptr(), ImGuiColorEditFlags_Float);
-			trail->modified_by_user |= ImGui::DragFloat("Color Fade time", &trail->color_fade_length, 0.01f, 0.0f, 10.0F);
-			if (!trail->fade_between_colors)
-			{
-				ImGui::PopItemFlag();
-				ImGui::PopStyleVar();
-			}
 		}
 	}
 }
