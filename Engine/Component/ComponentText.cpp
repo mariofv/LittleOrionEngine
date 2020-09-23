e5cbafcc6511477b172744d45cc9ddfb775b3592
Mario Fernández Villalba <mario_bcn2@hotmail.es>
2020-09-23 02:12:14 +0200

    Fix bug where ComponentText ws not rendered correctly

--- Engine/Component/ComponentText.cpp
+++ Engine/Component/ComponentText.cpp
@@ -32,25 +32,34 @@ ComponentText::~ComponentText()
 void ComponentText::InitData()
 {
 	GLfloat vertices[] = {
-		// Pos      // Tex
-		0.f, 1.f, 0.0f, 0.0f,
-		1.f, 0.f, 1.0f, 1.0f,
-		0.f, 0.f, 0.0f, 1.0f,
+		// Pos      // Tex	
+		0.f, 1.f, 0.f, 0.0f, 0.0f,
+		1.f, 0.f, 0.f, 1.0f, 1.0f,
+		0.f, 0.f, 0.f, 0.0f, 1.0f,
 
-		0.f, 1.f, 0.0f, 0.0f,
-		1.f, 1.f, 1.0f, 0.0f,
-		1.f, 0.f, 1.0f, 1.0f
+		0.f, 1.f, 0.f, 0.0f, 0.0f,
+		1.f, 1.f, 0.f, 1.0f, 0.0f,
+		1.f, 0.f, 0.f, 1.0f, 1.0f
 	};
 
+	glGenBuffers(1, &ebo);
 	glGenVertexArrays(1, &vao);
 	glGenBuffers(1, &vbo);
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 	glBindVertexArray(vao);
+
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 }
