49bb17a79469595189f904e4e45e97d35e621a30
Ana Hernández <anaisabel14hb@gmail.com>
2020-10-03 22:20:01 +0200

    remove extra code

--- Game/Resources/Shaders/PostProcess/postprocess.fs
+++ Game/Resources/Shaders/PostProcess/postprocess.fs
@@ -36,68 +36,33 @@ const float RAY_STEP = 0.05; // 0.025
 const float MIN_RAY_STEP = 0.2;
 
 // Transform hit_coordinate to screen coordinate
-void GetCoordinatesInScreenSpace(in vec3 in_coordinates, inout vec4 out_coordinates)
+vec4 GetCoordinatesInScreenSpace(vec3 in_coordinates)
 {
-    out_coordinates = matrices.proj * vec4(in_coordinates, 1.0) ;
-    out_coordinates.xyz /= out_coordinates.w;
+    vec4 out_coordinates = matrices.proj * vec4(in_coordinates, 1.0);
+    out_coordinates.xy /= out_coordinates.w;
     out_coordinates.xy = out_coordinates.xy * 0.5 + 0.5;
-}
-vec3 BinarySearch(inout vec3 direction, inout vec3 hit_coordinate, inout float depth_diff) {
-    
-	float depth;
-    vec4 projectedCoord;
- 
-    for(int i = 0; i < MAX_BINARY_SEARCH_COUNT; i++) {
-		GetCoordinatesInScreenSpace(hit_coordinate, projectedCoord);
-        depth = textureLod(positionMap, projectedCoord.xy,2.0).z;
- 
-		//search
-        depth_diff = hit_coordinate.z - depth;
-
-		direction *= 0.5;
-        if(depth_diff > 0.0)
-		{
-            hit_coordinate += direction;
-		}
-        else
-		{
-            hit_coordinate -= direction;  
-		}	
-    }
-
-    GetCoordinatesInScreenSpace(hit_coordinate, projectedCoord);
- 
-    return vec3(projectedCoord.xy, depth);
+	return clamp(out_coordinates,0,1);
 }
 
-vec4 RayCast(in vec3 direction, inout vec3 hit_coordinate, out float depth_diff) {
-
-    direction *= RAY_STEP;
-
-	float depth;
-    vec4 projectedCoord;
+vec4 RayCast(vec3 direction,vec3 hit_coordinate) {
 
     //Increase ray lenght until we find something
     for (int i = 0; i < MAX_NUMBER_INCREMENTS; i++) {
-        hit_coordinate += direction;
-        GetCoordinatesInScreenSpace(hit_coordinate, projectedCoord);
 
-        depth = textureLod(positionMap, projectedCoord.xy,2.0).z;
-		if(depth > 1000.0)
-		{
-			continue;
-		}
+        //hit_coordinate += direction;
+		vec4 projectedCoord = GetCoordinatesInScreenSpace(hit_coordinate);
 
-        depth_diff = hit_coordinate.z - depth;
-
+		float depth = texture(positionMap, projectedCoord.xy).z;
+		float depth_diff = hit_coordinate.z - depth;
 		//Avoid going over the depth diferent threshold  and check if we have hit something
-        if ((direction.z - depth_diff) < 1.2 && depth_diff <= 0.0) 
-		{
-            return  vec4(projectedCoord.xy, depth,0.0);//vec4(BinarySearch(direction, hit_coordinate, depth_diff),1.0);
+        if ((direction.z - depth_diff) < 1.2) 
+		{	
+			//return  vec4(matrices.proj[0][0],matrices.proj[0][0], 0,0.0);
+           return  vec4(vec2(projectedCoord),depth, 0.0);
 		}
     }
 
-    return vec4(projectedCoord.xy, depth,0.0);
+    return vec4(0, 0,0,0.0);
 }
 
 void main()
@@ -167,38 +132,20 @@ vec3 Uncharted2Tonemap(vec3 x)
    return ((x*(A*x+C*B)+D*E)/(x*(A*x+B)+D*F))-E/F;
 }
 
-vec3 FresnelSchlick(float cosTheta, vec3 F0)
-{
-    return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
-}
-
 vec3 Reflections(float reflection_strength)
 {
 #ifndef ENABLE_MSAA
-	mat4 invese_view = inverse(matrices.view);
 
 	vec3 view_normal = vec3(texture(normalMap, texCoord));
-	vec3 view_position = texture(positionMap, texCoord).xyz;
-	vec3 diffuse = texture(screen_texture, texCoord).xyz;
-
-	/*vec3 F0 = vec3(0.04); 
-    F0      = mix(F0, diffuse, reflection_strength);
-    vec3 Fresnel = FresnelSchlick(max(dot(normalize(view_normal), normalize(view_position)), 0.0), F0);*/
+	vec3 view_position = vec3(texture(positionMap, texCoord));
 
 	//Reflection
-	vec3 reflected = normalize(reflect(normalize(view_position), normalize(view_normal)));
+	vec3 reflected = normalize(reflect(normalize(view_position), normalize(view_normal))) ;
    
 	vec3 hit_position = view_position;
-	float depth_diff = 0.0;
-	vec4 coords = RayCast(reflected * max(MIN_RAY_STEP, 1.0 -view_position.z), hit_position,depth_diff);
+	vec4 coords = RayCast(reflected, hit_position);
 
-	//Fade edged and ignore results outside screen
-	vec2 diff_coords = smoothstep(0.2, 0.6, abs(vec2(0.5, 0.5) - coords.xy));
-	float screen_edge_factor = clamp(1.0 -(diff_coords.x + diff_coords.y), 0.0, 1.0);
-	float Reflection_multiplier = pow(reflection_strength, 3.0) * 
-            screen_edge_factor;
-
-	vec3 SSR = texture(screen_texture, coords.xy).xyz * clamp(Reflection_multiplier, 0.0, 0.9);
+	vec3 SSR = texture(screen_texture, coords.xy).xyz;
 
 	return SSR;
  #endif
